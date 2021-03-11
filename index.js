a02ef4695d0e69080fbd15927e7b5c686477dda2
mshaffer <mshaffer0308@gmail.com>
2021-03-11 10:11:27 -0600

    Changed JS file

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let animePic = document.getElementById('anime-pic');
 let handleClick = function() {
     fetch(`https://api.jikan.moe/v3/anime/1`).then(res => res.json())
     .then(data => {
-        animeDiv.innerHTML += `<p> ${data.title} </p>`
+        animeDiv.innerHTML = `<p> ${data.title} </p>`
         animePic.innerHTML = `<img src = "${data.image_url}" />`
     });
 }
