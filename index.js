e49397cda1467f4bd6c63d6852e70078e1c7d987
mshaffer <mshaffer0308@gmail.com>
2021-03-11 10:07:56 -0600

    Added javascript functionality

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 let animeDiv = document.getElementById('anime');
+let animePic = document.getElementById('anime-pic');
 
 let handleClick = function() {
     fetch(`https://api.jikan.moe/v3/anime/1`).then(res => res.json())
     .then(data => {
         animeDiv.innerHTML += `<p> ${data.title} </p>`
+        animePic.innerHTML = `<img src = "${data.image_url}" />`
     });
 }
-
+ 
